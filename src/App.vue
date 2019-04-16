5fa524ca2fecd30f1778bfa230c10e6acf8b0a2d
shaoshanhuan <shaoshanhuan@163.com>
2019-04-16 17:14:03 +0800

    配置好了ESLint

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    <h1>你好，世界</h1>
-  </div>
+	<div>
+		<h1>你好，世界</h1>
+	</div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+    methods:{
+        haha(){
+            var a = 10;
+            var b = 20;
+            a++;
+            console.log(a);
+            console.log('我爱你');
+            b++;
+            console.log(b);
+        }
+    }
+};
 </script>
 
 <style lang="less" scoped>
